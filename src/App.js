bca60553d50f5a46d3d1b9a4a014f0ba37789590
isaac <code.isaacfernandes@gmail.com>
2023-01-21 01:15:30 -0500

    Not sure what this even is

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-function App() {
+const App = () => {
   return (
-    <div className="text-center">
-      <header className="bg-slate-700 min-h-screen flex items-center justify-center text-neutral-50 text-2xl">
+    <div class="text-center">
+      <header class="bg-slate-700 min-h-screen flex items-center justify-center text-neutral-50 text-2xl">
         <p>
-          <p className="text-3xl font-bold">
+          <p class="text-3xl font-bold">
             Hello World!
           </p>
-          <p className="italic">
+          <p class="italic">
             By Isaac
           </p>
         </p>
